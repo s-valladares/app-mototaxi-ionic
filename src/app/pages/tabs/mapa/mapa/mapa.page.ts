35374e7f078bc0a0e0dcd35d589e8bd67fdafa69
Salvador Valladares <salva.1416@gmail.com>
2019-11-15 22:28:46 -0600

    Instalar geolocalizacion, obtener geolocalizacion

--- src/app/pages/tabs/mapa/mapa/mapa.page.ts
+++ src/app/pages/tabs/mapa/mapa/mapa.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-mapa',
   templateUrl: 'mapa.page.html',
   styleUrls: ['mapa.page.scss']
 })
-export class MapaPage {
+export class MapaPage implements OnInit {
 
-  constructor() {}
+  constructor(
+    private geolocation: Geolocation
+  ) { }
+
+  ngOnInit() {
+    this.getLocation();
+  }
+
+  getLocation() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      console.log(resp.coords.latitude);
+      console.log(resp.coords.longitude);
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
+
+  watchLocation() {
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
+    });
+  }
 
 }
