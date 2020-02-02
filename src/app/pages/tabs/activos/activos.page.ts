13d42fbd6b416721883081f6d1cf9f75271394f0
Salvador Valladares <salva.1416@gmail.com>
2020-02-01 21:27:04 -0600

    SALVA: botones  activos

--- src/app/pages/tabs/activos/activos.page.ts
+++ src/app/pages/tabs/activos/activos.page.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivosPage implements OnInit {
 
-  constructor() { }
+  clCercanos: boolean;
+  clPreferidos: boolean;
+
+  constructor() {
+    this.clCercanos = true;
+    this.clPreferidos = false;
+  }
 
   ngOnInit() {
   }
+
+  clickCercanos() {
+    this.clPreferidos = false;
+    this.clCercanos = true;
+    console.log('Cerca: ', this.clCercanos);
+    console.log('Preferidos: ', this.clPreferidos);
+  }
+
+  clickPreferidos() {
+    this.clCercanos = false;
+    this.clPreferidos = true;
+    console.log('Cerca: ', this.clCercanos);
+    console.log('Preferidos: ', this.clPreferidos);
+  }
+
 
 }
